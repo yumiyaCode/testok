66450179c247eca62884be4278ff9663e37bd947
Diki Maulana <tyrantsid11@gmail.com>
2021-03-19 08:26:01 +0700

    seed user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class,
-            ProvinsiSeeder::class,
-            KotaSeeder::class
-          
-	       
+            UserSeeder::class 
     	]);
     }
 }
